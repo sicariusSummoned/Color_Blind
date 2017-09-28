819dcf981724091999b39ab7f2e3db32e6403119
Dante Nardo <dante.nardo@outlook.com>
2017-09-28 19:59:31 -0400

    Summary: Added the ColorManager Singleton. Detailed: This color manager contains important color information that we will be updating. This manager will eventually include references to different types of sprites and update them accordingly.

--- Project Colorblind/Assets/Scripts/ColorCalibration.cs
+++ Project Colorblind/Assets/Scripts/ColorCalibration.cs
@@ -2,7 +2,7 @@
 
 /// <summary>
 /// Author: Dante Nardo
-/// Last Modified: 9/3/2017
+/// Last Modified: 9/28/2017
 /// Purpose: Calibrates the correct value for each material used in-game.
 /// </summary>
 public class ColorCalibration : MonoBehaviour
@@ -11,14 +11,8 @@ public class ColorCalibration : MonoBehaviour
     public Renderer m_cubeRenderer;
     public Material[] m_materials;
     private Material m_current;
-    private Color m_temp;           // Saves memory
+    private Color m_temp;
     private int m_index;
-
-    #region Default Color Values
-    private const float RedOpacity = 0.9f;
-    private const float GreenOpacity = 0.125f;
-    private const float BlueOpacity = 0.25f;
-    #endregion
     #endregion
 
     #region ColorCalibration Methods
@@ -27,9 +21,9 @@ public class ColorCalibration : MonoBehaviour
         m_index = 0;
         m_current = m_materials[0];
 
-        SetOpacity(m_materials[0], RedOpacity);
-        SetOpacity(m_materials[1], GreenOpacity);
-        SetOpacity(m_materials[2], BlueOpacity);
+        m_materials[0].color = ColorManager.Instance.MasterRed;
+        m_materials[1].color = ColorManager.Instance.MasterGreen;
+        m_materials[2].color = ColorManager.Instance.MasterBlue;
     }
 
     void Update()
@@ -44,14 +38,14 @@ public class ColorCalibration : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             m_temp = m_current.color;
-            m_temp.a -= 0.01f;
+            m_temp.a -= 0.005f;
             m_current.color = m_temp;
             return;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             m_temp = m_current.color;
-            m_temp.a += 0.01f;
+            m_temp.a += 0.005f;
             m_current.color = m_temp;
             return;
         }
@@ -64,16 +58,28 @@ public class ColorCalibration : MonoBehaviour
 
     private void NextMaterial()
     {
+        // Update master color
+        switch (m_index)
+        {
+            case 0:
+                ColorManager.Instance.UpdateMasterRed(m_current.color);
+                break;
+            case 1:
+                ColorManager.Instance.UpdateMasterGreen(m_current.color);
+                break;
+            case 2:
+                ColorManager.Instance.UpdateMasterBlue(m_current.color);
+                break;
+            default:
+                return;
+        }
+
         m_index++;
+        if (m_index >= m_materials.Length)
+            return;
+
         m_current = m_materials[m_index];
         m_cubeRenderer.material = m_current;
-    }
-
-    private void SetOpacity(Material material, float opacity)
-    {
-        m_temp = material.color;
-        m_temp.a = opacity;
-        material.color = m_temp;
     }
     #endregion
 }
---
+++ Project Colorblind/Assets/Scripts/ColorManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Author: Dante Nardo
+/// Last Modified: 9/28/2017
+/// Purpose: A singleton that holds all of the global color information.
+/// </summary>
+public class ColorManager : MonoBehaviour
+{
+    #region ColorManager Members
+    public static ColorManager Instance;
+
+    private Color m_defaultRed =   new Color(1,    0, 0,    0.5f);
+    private Color m_defaultGreen = new Color(0.1f, 1, 0.1f, 0.3f);
+    private Color m_defaultBlue =  new Color(0.2f, 0, 1,    0.3f);
+
+    private Color m_masterRed;
+    private Color m_masterGreen;
+    private Color m_masterBlue;
+
+    public Color MasterRed
+    {
+        get { return m_masterRed; }
+    }
+    public Color MasterGreen
+    {
+        get { return m_masterGreen; }
+    }
+    public Color MasterBlue
+    {
+        get { return m_masterBlue; }
+    }
+    #endregion
+
+    #region ColorManager Methods
+    private void Awake()
+    {
+        // Destroy if there are multiple instances
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+
+        // Stay persistent between levels
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        m_masterRed = m_defaultRed;
+        m_masterGreen = m_defaultGreen;
+        m_masterBlue = m_defaultBlue;
+    }
+
+    public void UpdateMasterRed(Color c)
+    {
+        m_masterRed = c;
+    }
+
+    public void UpdateMasterGreen(Color c)
+    {
+        m_masterGreen = c;
+    }
+
+    public void UpdateMasterBlue(Color c)
+    {
+        m_masterBlue = c;
+    }
+    #endregion
+}
