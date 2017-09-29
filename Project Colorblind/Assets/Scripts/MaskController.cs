3ad61f0ebbe9e9ffd8a3cdd9d29a1e2e2510288f
James Taggart <jxt1607@g.rit.edu>
2017-09-29 10:32:47 -0400

    Summary: make my mask controller work using inputs instead of just keydown Details: Created bug where pressing button to switch masks acts as rapid-fire

--- Project Colorblind/Assets/Scripts/MaskController.cs
+++ Project Colorblind/Assets/Scripts/MaskController.cs
@@ -2,31 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeMask : MonoBehaviour {
+public class MaskController : MonoBehaviour {
+    public enum MASK_COLOR { Red, Green, Blue };
+    public MASK_COLOR myColor;
     public Sprite[] sprites;
     private SpriteMask myMask;
+    private Transform myTransform;
     private int index = 0;
 
 
 	// Use this for initialization
 	void Start () {
         myMask = GetComponent<SpriteMask>();
+        myTransform = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyUp(KeyCode.Space))
+        switch (myColor)
         {
-            CycleActiveSprite();
-        }else if (Input.GetKeyUp(KeyCode.Alpha1))
-        {
-            SetActiveSprite(0);
-        }else if (Input.GetKeyUp(KeyCode.Alpha2))
-        {
-            SetActiveSprite(1);
-        }else if (Input.GetKeyUp(KeyCode.Alpha3))
-        {
-            SetActiveSprite(2);
+            case MASK_COLOR.Red:
+                if (Input.GetAxis("P1_CycleMask") > 0)
+                {
+                    CycleActiveSprite();
+                }
+                break;
+            case MASK_COLOR.Green:
+                if(Input.GetAxis("P2_CycleMask") > 0)
+                {
+                    CycleActiveSprite();
+                }
+                break;
+            case MASK_COLOR.Blue:
+                if(Input.GetAxis("P3_CycleMask")> 0)
+                {
+                    CycleActiveSprite();
+                }
+                break;
         }
 	}
 
