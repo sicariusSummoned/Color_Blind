2f2157470ac7b3c36e86ee782fdf82d636a8e60f
Dante Nardo <dante.nardo@outlook.com>
2017-10-01 20:50:13 -0400

    Summary: Added randomized traitor text. Detailed: Fixed the enemy patrol and modified level slightly as well as adding a gameobject that randomly determines the traitor.

--- Project Colorblind/Assets/Scripts/EnemyAI.cs
+++ Project Colorblind/Assets/Scripts/EnemyAI.cs
@@ -1,47 +1,53 @@
 ﻿//based on Scripting is Fun's Youtube Tutorial - "Unity 2D Game Basics - Enemy AI - Patrolling"
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour {
-
+/// <summary>
+/// Author: Dante Nardo
+/// Last Modified: 10/1/2017
+/// Purpose: Moves an enemy between patrol points.
+/// </summary>
+public class EnemyAI : MonoBehaviour
+{
+    #region EnemyAI Members
     public Transform[] patrolPoints;
     public float speed;
     Transform currentPatrolPoint;
     int currentPatrolIndex;
+    #endregion
 
-
-	// Use this for initialization
-	void Start () {
+    #region EnemyAI Methods
+    void Start()
+    {
         currentPatrolIndex = 0;
         currentPatrolPoint = patrolPoints[currentPatrolIndex];
-	}
-	
-	// Update is called once per frame
-	void Update () {
-       transform.Translate(Vector3.up * Time.deltaTime * speed);
-        //check to see if enemy has reached patrol point. If so switch to next patrol point.
-        if(Vector3.Distance (transform.position, currentPatrolPoint.position) < .1f)
+    }
+
+    void Update()
+    {
+        // Move towards patrol point
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+
+        // Switch to next patrol point if passed current
+        if (Vector3.Distance(transform.position, currentPatrolPoint.position) < .1f)
         {
-            if(currentPatrolIndex +1 < patrolPoints.Length)
-            {
+            if (currentPatrolIndex + 1 < patrolPoints.Length)
                 currentPatrolIndex++;
-            }
             else
-            {
                 currentPatrolIndex = 0;
-            }
+
             currentPatrolPoint = patrolPoints[currentPatrolIndex];
         }
 
-        //turn to face current control patrol point
-        //Finding the direction Vector that points to the patrol point
+        // Turn to face current control patrol point
+        // Finding the direction Vector that points to the patrol point
         Vector3 patrolPointDir = currentPatrolPoint.position - transform.position;
-        //figure out rotation in degrees to turn enemy
-        float angle = Mathf.Atan2(patrolPointDir.y, patrolPointDir.x) * Mathf.Rad2Deg - 90f;
+
+        // Figure out rotation in degrees to turn enemy
+        float angle = Mathf.Atan2(patrolPointDir.y, patrolPointDir.x) * Mathf.Rad2Deg;
 
         Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180f);
-	}
+    }
+    #endregion
 }
