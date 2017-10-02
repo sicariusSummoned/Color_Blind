1963bc0103acaa751db85a6b0da26708ffd5bba4
Rochelle Cohn (RIT Student) <rc7604@ad.rit.edu>
2017-10-02 12:30:42 -0400

    Summary: Camera now follows a centroid object which follows the middle of the players. MUST GET RID OF PLAYER FALLING INTO THE ABYSS.

---
+++ Project Colorblind/Assets/Scripts/Centroid.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Centroid : MonoBehaviour {
+
+    // Use this for initialization
+    private Vector3 centroid;
+
+    public Camera mainCamera;
+
+    public Vector3 Centroids
+    {
+        get { return centroid; }
+    }
+    public int numPlayers;
+
+    private GameObject[] players;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        CalcCentroid();
+        this.transform.position = centroid;
+    
+	}
+
+
+    private void CalcCentroid()
+    {
+        float x = 0;
+        float y = 0;
+
+        for(int i=0; i < players.Length; i++ )
+        {
+            x += players[i].transform.position.x;
+            y += players[i].transform.position.y;
+        }
+        numPlayers = players.Length;
+        x = x / numPlayers;
+        y = y / numPlayers;
+
+        centroid = new Vector3(x, y, -10);
+
+    }
+
+
+
+}
