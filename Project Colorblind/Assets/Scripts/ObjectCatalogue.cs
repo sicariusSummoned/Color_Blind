9fd7e892286624c6fb1d472cc42ab8c4a162cc1a
James Taggart <jxt1607@g.rit.edu>
2017-10-03 16:41:42 -0400

    Summary: Created branch. Details: Moved old platforms to Resources folder, Created ColorLaser.cs, Created ObjectCatalogue.cs. Basic idea is to raycast out, onhit determine what kind of block it is and replace it with another block of the same type but color of the user.

---
+++ Project Colorblind/Assets/Scripts/ObjectCatalogue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCatalogue : MonoBehaviour
+{
+    public enum BLOCK_COLOR { Red, Green, Blue };
+    public BLOCK_COLOR myColor;
+
+    public enum BLOCK_TYPE { Platform, MovingPlatform, Spikes };
+    public BLOCK_TYPE myType;
+}
