96aead13564bf20cd73096baec25cdf5480a96c4
Dante Nardo <dante.nardo@outlook.com>
2017-10-24 21:35:35 -0400

    Summary: Added ghost players and fixed level manager. Detailed: Removed necessary keypress from level manager and added players that fly around without collisions.

---
+++ Project Colorblind/Assets/2D Platformer Controller/Scripts/GhostController2D.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Author: Dante Nardo
+/// Last Modified: 10/24/17
+/// Purpose: A ghost controller that moves the ghost through everything.
+/// </summary>
+public class GhostController2D : MonoBehaviour
+{
+    #region GhostController2D Methods
+    public void Move(Vector2 moveAmount)
+    {
+        transform.Translate(moveAmount);
+    }
+    #endregion
+}
--- Project Colorblind/Assets/Scripts/LevelManager.cs
+++ Project Colorblind/Assets/Scripts/LevelManager.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Author: Dante Nardo
-/// Last Modified: 10/4/2017
+/// Last Modified: 10/24/2017
 /// Purpose: Moves player from one level to the next.
 /// </summary>
 public class LevelManager : MonoBehaviour
@@ -11,18 +11,15 @@ public class LevelManager : MonoBehaviour
     #region LevelManager Members
     public string m_nextLevel;
     public float m_minDist;
-    public Player m_player1;
-    public Player m_player2;
-    public Player m_player3;
+    public Player[] m_players;
     #endregion
 
     #region LevelManager Methods
     void Update()
     {
         // Janky Solution
-        // Checks if a player is nearby during keypress
         // Sends player to next level
-        if (Input.GetKey(KeyCode.Space) && PlayerNear())
+        if (PlayerNear())
         {
             SceneManager.LoadScene(m_nextLevel);
         }
@@ -30,9 +27,11 @@ public class LevelManager : MonoBehaviour
 
     public bool PlayerNear()
     {
-        return (Vector3.Distance(transform.position, m_player1.transform.position) < m_minDist ||
-                Vector3.Distance(transform.position, m_player2.transform.position) < m_minDist ||
-                Vector3.Distance(transform.position, m_player3.transform.position) < m_minDist);
+        foreach (var player in m_players)
+            if (Vector3.Distance(transform.position, player.transform.position) < m_minDist)
+                return true;
+
+        return false;
     }
     #endregion
 }
