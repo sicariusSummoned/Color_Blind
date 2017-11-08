529777edb50bf7594b24bce3ebe195213c59bb47
Sulfur <rc7604@g.rit.edu>
2017-11-07 21:01:31 -0500

    Summary: Got pause menu for the most part working. For some reason button does not detect there is a pause function so clicking unpause does not work for now.

---
+++ Project Colorblind/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+	public Transform canvas;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+
+			Pause ();
+		}
+
+	}
+
+
+	public void Pause(){
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (canvas.gameObject.activeInHierarchy == false) {
+				canvas.gameObject.SetActive (true);
+				Time.timeScale = 0;
+
+
+
+			} else {
+				canvas.gameObject.SetActive (false);
+				Time.timeScale = 1;
+			}
+
+	}
+			
+}
