d1caf64d931a2182c62c1d5a4814bb9cb3fc48fd
Rochelle Cohn (RIT Student) <rc7604@ad.rit.edu>
2017-11-09 16:19:37 -0500

    Summary: Basic main menu. Got buttons to click on main menu and on pause menu

---
+++ Project Colorblind/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+    public Canvas warningMenu;
+    public Canvas mainMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //clicking the understand button to go to main menu
+    public void ToMainMenu()
+    {
+        mainMenu.gameObject.SetActive(true);
+        warningMenu.gameObject.SetActive(false);
+    }
+
+    public void play()
+    {
+        SceneManager.LoadScene("1_1", LoadSceneMode.Single);
+    }
+
+    public void quitting()
+    {
+        Application.Quit();
+    }
+}
---
+++ Project Colorblind/Assets/Scripts/testButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void clickButon()
+    {
+        Debug.Log("clicked");
+    }
+}
