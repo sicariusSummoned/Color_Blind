403031fdabc619c4b8e10bca8df9ad1bfc59a001
DanteNardo <dcn5783@g.rit.edu>
2017-11-17 00:16:21 -0500

    Summary: Pause menu now launches working calibration menu.

--- Project Colorblind/Assets/Scripts/PauseGame.cs
+++ Project Colorblind/Assets/Scripts/PauseGame.cs
@@ -1,55 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseGame : MonoBehaviour {
-	public Transform canvas;
-
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-
-			Pause ();
-		}
-
-	}
+public class PauseGame : MonoBehaviour
+{
+    public GameObject pauseCanvas;
+    public GameObject calibrationCanvas;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
 
 
     public void Pause()
     {
-        Debug.Log("PAUSE HAS BEEN USED");
-
-            if (canvas.gameObject.activeInHierarchy == false)
-            {
-                canvas.gameObject.SetActive(true);
-                Time.timeScale = 0;
-
-                Debug.Log("Here");
-            }
-            else
-            {
-                Debug.Log("There");
-                Time.timeScale = 1;
-                canvas.gameObject.SetActive(false);
-                
-            }      
+        if (pauseCanvas.activeInHierarchy == false)
+        {
+            pauseCanvas.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseCanvas.SetActive(false);
+        }
     }
-		
-	public void returnMain(){
-		SceneManager.LoadScene ("main_menu", LoadSceneMode.Single);
-	}
-		
-	public void quitting()
-	{
-		Application.Quit();
-	}
+
+    public void ActivateCalibration()
+    {
+        calibrationCanvas.SetActive(true);
+        pauseCanvas.SetActive(false);
+    }
+
+    public void DectivateCalibration()
+    {
+        calibrationCanvas.SetActive(false);
+        pauseCanvas.SetActive(true);
+    }
+
+    public void ReturnMain()
+    {
+        SceneManager.LoadScene("main_menu", LoadSceneMode.Single);
+    }
+
+    public void Quitting()
+    {
+        Application.Quit();
+    }
 
 }
