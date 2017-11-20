b2c367c911898fe816613a45927ccaecbf9444f6
Sulfur <rc7604@g.rit.edu>
2017-11-19 21:03:07 -0500

    Summary: Working on loading in levels

---
+++ Project Colorblind/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LoadLevel : MonoBehaviour {
+
+	public GameObject loadingScreen; 
+	public Slider slider; 
+
+	//used for loading in levels. Name is the name of the level
+	public void LoadInLevel(string levelName){
+		StartCoroutine (loadAsync (levelName));
+	}
+
+	IEnumerator loadAsync(string levelName){
+		AsyncOperation op =	SceneManager.LoadSceneAsync (levelName);
+		loadingScreen.SetActive (true);
+		while (op.isDone == false) {
+			float progress = Mathf.Clamp01 (op.progress / .9f);
+			slider.value = progress;
+
+			yield return null;
+
+		}
+
+	}
+
+
+}
